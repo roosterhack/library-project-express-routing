1f97810de0a2529fbe7f1d4386ff996b4d6cce16
Eddie Chung <designered@gmail.com>
2019-04-15 14:51:43 +0200

    added editing book, update database functions

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,31 @@ router.post("/books/add", (req, res) => {
     });
 });
 
+//update book info after editing
+router.post("/book/edit/:bookId", (req, res) => {
+  const _id = req.params.bookId;
+  const { title, author, description, rating } = req.body;
+  Book.findOneAndUpdate({ _id }, { title, author, description, rating }, { new: true })
+    .then(updatedData => {
+      res.redirect(`/books/${updatedData._id}`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+//edit a book
+router.get("/book/edit/:bookId", (req, res) => {
+  const _id = req.params.bookId;
+  Book.findOne({ _id })
+    .then(book => {
+      res.render("book-edit", { book });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 //Get book details
 router.get("/books/:bookId", (req, res) => {
   const _id = req.params.bookId;
